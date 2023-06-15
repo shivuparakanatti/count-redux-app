3893ab567d3afbdeb8ea4dca099a5ddb44e92512
shivuparakanatti <103122119+shivuparakanatti@users.noreply.github.com>
2023-06-15 22:31:02 +0530

    Update

--- src/btn.js
+++ src/btn.js
@@ -12,7 +12,6 @@ return (
                 dispatch(increment())
             }}>+1</button>
         
-        
             <button onClick={()=>{
                 dispatch(decrement())
             }}>-1</button>
